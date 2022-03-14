171dae04d86cc45be58dac64ab87a8c1ad8994c5
Leo Liu <leozxc2009@gmail.com>
2022-03-14 15:02:27 +0800

    0314 add Strategy table information

--- chart0302/Controllers/stocksController.cs
+++ chart0302/Controllers/stocksController.cs
@@ -21,7 +21,8 @@ namespace chart0302.Controllers
             return View();
         }
 
-        public ActionResult chart(int? id)
+
+        public ActionResult Chart(string id)
         {
             ViewBag.id = id;
             if(id == null)
@@ -30,12 +31,12 @@ namespace chart0302.Controllers
             }
             else
             {
-                return View(db.stocks.Where(x => x.stockID == id.ToString()).OrderBy(x => x.stockDate).ToList());
+                return View(db.stocks.Where(x => x.stockID == id).OrderBy(x => x.stockDate).ToList());
             }
         }
 
         [HttpPost]
-        public ActionResult chart(string stockID)
+        public ActionResult Chart(string stockID, string id)
         {
             ViewBag.id = stockID;
             if (stockID == "")
@@ -48,29 +49,49 @@ namespace chart0302.Controllers
             }
         }
 
-        public ActionResult strategy(string str1)
+        public ActionResult Strategy(string str)
         {
             string id = "Strategy";
             string stringID = "";
             string stockArray =  "";
-            if (str1 == "確認")
+            switch (str)
             {
-                //foreach (string s in db.stocks.Select(x => x.stockID).Distinct().OrderBy(x => x))
-                //{
-                //    var dbs = db.stocks.Where(x => x.stockID == s).ToList();
-                //    try
-                //    {
-                //        if (float.Parse(dbs.Where(x => x.stockDate == "20210409").Select(x => x.endPrice).ToList()[0]) > 900)
-                //        {
-                //            stringID = stringID + s + "\n";
-                //        }
-                //    }
-                //    catch
-                //    {
-                //    }
-                //}
-                stockArray += ", 1439";
-                stockArray += ", 2330";
+                case "策略一":
+                    //foreach (string s in db.stocks.Select(x => x.stockID).Distinct().OrderBy(x => x))
+                    //{
+                    //    var dbs = db.stocks.Where(x => x.stockID == s).ToList();
+                    //    try
+                    //    {
+                    //        if (float.Parse(dbs.Where(x => x.stockDate == "20210409").Select(x => x.endPrice).ToList()[0]) > 900)
+                    //        {
+                    //            stringID = stringID + s + "\n";
+                    //        }
+                    //    }
+                    //    catch
+                    //    {
+                    //    }
+                    //}
+                    stockArray += ", 2303";
+                    stockArray += ", 2330";
+                    break;
+                case "策略二":
+                    stockArray += ", 0050";
+                    stockArray += ", 2603";
+                    break;
+                case "策略三":
+                    stockArray += ", 2609";
+                    stockArray += ", 2409";
+                    break;
+                case "策略四":
+                    stockArray += ", 2610";
+                    stockArray += ", 2618";
+                    break;
+                case "策略五":
+                    stockArray += ", 3481";
+                    stockArray += ", 2002";
+                    break;
+                default:
+                    break;
             }
             ViewBag.id = id;
             ViewBag.stringID = stringID;
